b8576445370bd9aeb25020df2ce9cb500d1c0077
AronNascimento2 <120020442+AronNascimento2@users.noreply.github.com>
2023-08-26 10:57:39 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { HomePage } from './presentation/Pages/HomePage'
+import { Header } from './presentation/components/Header'
+import './app.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HomePage />
+    </>
+  )
+}
+
+export default App
